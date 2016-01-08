a83a475b12d8ec4c9eeebada69b0a4efdf31b5ef
root <root@blog-wordpress.c.powerful-bounty-113521.internal>
2016-01-08 00:31:04 +0000

    fixing header

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	
 	<?php wp_head(); ?>
 </head>
 
