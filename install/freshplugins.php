fc8e0b8557a3e7d9e99dc13e9df83b4aea0cc83d
gideon <gideonbrosenthal@gmail.com>
2016-10-16 22:55:12 -0700

    adding

---
+++ install/freshplugins.php
@@ -0,0 +1,11 @@
+<?php
+
+$freshplugins = array(
+	'zero-core',
+	'fresh-custom-code',
+	'fresh-custom-loops',
+	'fresh-file-editor',
+	'fresh-performance-cache',
+	'fresh-scrollbar',
+	'fresh-sidebar-manager',
+);
